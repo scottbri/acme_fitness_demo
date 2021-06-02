a1fb68f0b8ba4cdf4e8692dbbd17eea9477b9b72
Scott Brightwell <scott@brightwell.org>
2021-06-02 11:01:34 -0500

    typo

--- 01-install-acme-fitness.sh
+++ 01-install-acme-fitness.sh
@@ -1,5 +1,6 @@
 NAMESPACE=acme-fitness
 APPNAME=${NAMESPACE}
+
 REDIS_PASS="superSecretPassword!"
 MONGO_PASS="superSecretPassword!"
 POSTGRESS_PASS="superSecretPassword!"
@@ -7,7 +8,8 @@ POSTGRESS_PASS="superSecretPassword!"
 
 kubectl create ns ${NAMESPACE}
 
-mkdir passwords
+mkdir secrets
+
 kubectl create secret generic cart-redis-pass --from-literal=password=${REDIS_PASS} --dry-run -oyaml > secrets/redis-pass.yaml
 kubectl create secret generic catalog-mongo-pass --from-literal=password=${MONGO_PASS} --dry-run -oyaml > secrets/mongo-pass.yaml
 kubectl create secret generic order-postgres-pass --from-literal=password=${POSTGRESS_PASS} --dry-run -oyaml > secrets/postgress-pass.yaml
