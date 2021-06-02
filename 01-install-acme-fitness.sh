52c2c482c3b792fb8cb342831e221e11e4bd3f46
Scott Brightwell <scott@brightwell.org>
2021-06-02 11:53:10 -0500

    into acme-fitness namespace

--- 01-install-acme-fitness.sh
+++ 01-install-acme-fitness.sh
@@ -1,17 +1,20 @@
 NAMESPACE=acme-fitness
 APPNAME=${NAMESPACE}
 
-REDIS_PASS="superSecretPassword!"
-MONGO_PASS="superSecretPassword!"
-POSTGRESS_PASS="superSecretPassword!"
+CART_PASS="superSecretPassword!"
+CATALOG_PASS="superSecretPassword!"
+ORDER_PASS="superSecretPassword!"
+USERS_PASS="superSecretPassword!"
 
 
 kubectl create ns ${NAMESPACE}
 
 mkdir secrets
 
-kubectl create secret generic cart-redis-pass --from-literal=password=${REDIS_PASS} --dry-run -oyaml > secrets/redis-pass.yaml
-kubectl create secret generic catalog-mongo-pass --from-literal=password=${MONGO_PASS} --dry-run -oyaml > secrets/mongo-pass.yaml
-kubectl create secret generic order-postgres-pass --from-literal=password=${POSTGRESS_PASS} --dry-run -oyaml > secrets/postgress-pass.yaml
+kubectl create secret generic cart-redis-pass --from-literal=password=${CART_PASS} --dry-run -oyaml > secrets/cart-redis-pass.yaml
+kubectl create secret generic catalog-mongo-pass --from-literal=password=${CATALOG_PASS} --dry-run -oyaml > secrets/catalog-mongo-pass.yaml
+kubectl create secret generic order-postgres-pass --from-literal=password=${ORDER_PASS} --dry-run -oyaml > secrets/-order-postgress-pass.yaml
+kubectl create secret generic users-mongo-pass --from-literal=password=${USERS_PASS} --dry-run -oyaml > secrets/users-mongo-pass.yaml
+kubectl create secret generic users-redis-pass --from-literal=password=${USERS_PASS} --dry-run -oyaml > secrets/users-redis-pass.yaml
 
-kapp deploy -a ${APPNAME} -n ${NAMESPACE} -f ./secrets/ -f ./kubernetes-manifests/ 
+kapp deploy -y -a ${APPNAME} -n ${NAMESPACE} -f ./secrets/ -f ./kubernetes-manifests/ 
